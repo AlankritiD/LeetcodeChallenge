d27d90d52ae633bc52bd94a005f42b661467c3e2
Alankriti Dadlani <76182821+AlankritiD@users.noreply.github.com>
2024-10-08 01:49:39 +0530

    Create 345.py

---
+++ 345.py
@@ -0,0 +1,16 @@
+#345. Reverse Vowels of a String
+def reverseVowel(self,s):
+  vowels=set('aeiouAEIOU')
+  s=list(s)
+  left=0
+  right=len(s)-1
+  while left<right:
+    while left<right and s[left] not in vowels:
+      left+=1
+    while left<right and s[right] not in   vowels:
+      right-=1
+    s[left],s[right]=s[right],s[left]
+    left+=1
+    right-=1
+  return ''.join(s)
+  
